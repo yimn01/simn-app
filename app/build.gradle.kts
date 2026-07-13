66da6fa330f96dbe64de1660c48e35997f7fbff2
yimn01 <65669090+yimn01@users.noreply.github.com>
2026-07-13 08:58:12 -0500

    Change applicationId in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,20 +3,17 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
-
 android {
     namespace = "com.quacko.billing"
     compileSdk = 34
-
     defaultConfig {
-        applicationId = "com.quacko.billing"
+        applicationId = "com.silvermindsllc.languageaccess"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         vectorDrawables { useSupportLibrary = true }
     }
-
     signingConfigs {
         create("release") {
             storeFile = rootProject.file("simn-release.keystore")
@@ -25,7 +22,6 @@ android {
             keyPassword = "SimnRelease2026!"
         }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -40,7 +36,6 @@ android {
             isDebuggable = true
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -61,25 +56,20 @@ android {
         }
     }
 }
-
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.09.00")
     implementation(composeBom)
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
     implementation("androidx.activity:activity-compose:1.9.2")
-
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.0")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
